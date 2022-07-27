e609f46b130606e819178032212cbd25e7bf4674
KnxPwr <gabrielebonumore@gmail.com>
2022-07-27 15:39:35 +0200

    Remove unused import

--- components/withdrawButton.tsx
+++ components/withdrawButton.tsx
@@ -4,7 +4,6 @@ import { useMigratorContract } from "hooks/useContract";
 import { useAppDispatch, useStoreState } from "hooks/useStore";
 import { thunkWithdraw } from "store/thunks";
 import Button from "./button";
-import { BigNumber } from "ethers";
 
 const WithdrawButton: React.FC = () => {
   const [state] = useStoreState();
